87e48cbb89fa1fcd899bde45143e4da29bc19173
Verónica Latapiat <47534582+VeronicaLatapiat@users.noreply.github.com>
2020-04-10 18:26:45 -0400

    Update SSN.m

--- SSN.m
+++ SSN.m
@@ -1,7 +1,4 @@
-#Functions operate on variables within their own workspace, which is also called the local workspace, separate from the workspace you access at the MATLAB command prompt which is called the base workspace.
-#Functions can accept more than one input arguments and may return more than one output arguments.
-#Syntax of a function statement is −
-#function [out1,out2, ..., outN] = myfun(in1,in2,in3, ..., inN)
+%%%%%%%% SSN method %%%%%%%%
 
 function [ index_R,p ] = SSN( sample,ref )
 %function:construct the SSN
@@ -11,29 +8,38 @@ function [ index_R,p ] = SSN( sample,ref )
 %   Output:
 %         adjacency_matrix:the network structure
 %a example
-# first dimension is all "row" and the second is "column"
-% sample=new_T(:,1); y a mi me interesan todas las columnas
-# entonces deberia ser sample=new_T(:,:);
+% sample=new_T(:,1);
 % ref=new_N;
 
-#[R,P] = corrcoef(___) returns the matrix of correlation coefficients and the matrix of p-values 
-#for testing the hypothesis that there is no relationship between the observed phenomena (null hypothesis).
-#Use this syntax with any of the arguments from the previous syntaxes. If an off-diagonal element of P is 
-#smaller than the significance level (default is 0.05), then the corresponding correlation in R is considered significant.
-#This syntax is invalid if R contains complex elements.
+% [R,P] = corrcoef(___) devuelve la matriz de coeficientes de correlación 
+% y la matriz de valores p para probar la hipótesis de que no existe relación
+% entre los fenómenos observados (hipótesis nula). Si un elemento 
+% OFF-diagonal de P es menor que el nivel de significancia (el valor 
+% predeterminado es 0.05), la correlación correspondiente en R se considera
+% significativa. Esta sintaxis no es válida si R contiene elementos complejos.
 
 [R,P]=corrcoef(ref');
-final_R0=R; # matriz de correlacion
-final_R0(isnan(final_R0))=0; # returns a logical array containing 1 (true) where the elements of A are NaN, and 0 (false) 
-                             # where they are not. If A contains complex numbers, isnan(A) contains 1 for elements with 
-                             # either real or imaginary part is NaN, and 0 for elements where both real and imaginary parts are not NaN.
+final_R0=R;
+final_R0(isnan(final_R0))=0;
 
+%disp("final_R0_0")
+%disp(final_R0)
+
+%NEW_data=[sample ref];
 NEW_data=[ref sample];
 [R1,P1]=corrcoef(NEW_data');
 final_R1=R1;
 final_R1(isnan(final_R1))=0;
 
 index_R=final_R1-final_R0;
+
+%disp("final_R1")
+%disp(final_R1)
+%disp("final_R0")
+%disp(final_R0)
+
+%disp(index_R)
+
 [m,n]=size(ref);
 Z=index_R./((1-final_R0.^2)/(n-1));
 Z(Z==inf)=max(max(Z));
@@ -41,6 +47,7 @@ Z(Z==-inf)=-max(max(Z));
 Z(isnan(Z))=0;
 
 clear NEW_data final_R1 final_R0 R0 R1 P P1
-p=1-normcdf(abs(Z)); # returns the cumulative distribution function (cdf) of the standard normal distribution, evaluated at the values in x.
+p=1-normcdf(abs(Z));
 
 end
+
